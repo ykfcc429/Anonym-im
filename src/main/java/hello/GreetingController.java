2e1e7a1ca205ba1812279de0afd201dcd55f13a7
yankaifeng <yankaifeng@hzzl.net>
2021-04-01 10:20:16 +0800

    Initial commit

---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,18 @@
+package hello;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class GreetingController {
+
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        return new Greeting(HtmlUtils.htmlEscape(message.getName()));
+    }
+
+}
