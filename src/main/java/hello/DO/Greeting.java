b8f6d0e5bb488ea53898f44e7b1bed83513b69f5
ykfcc429 <1141154320@qq.com>
2021-04-02 17:21:58 +0800

    update

--- src/main/java/hello/DO/Greeting.java
+++ src/main/java/hello/DO/Greeting.java
@@ -1,18 +1,14 @@
 package hello.DO;
 
+
+import lombok.Data;
+
+@Data
 public class Greeting {
 
     private String content;
 
-    public Greeting() {
-    }
-
     public Greeting(String content) {
         this.content = content;
     }
-
-    public String getContent() {
-        return content;
-    }
-
 }
